5cae8584a6942dce4c36d5b2d765f1642135fb04
youkou <youkousai@gmail.com>
2023-01-27 21:25:20 +0900

    chore: update webapp (--out table)

--- azure-webapp/azure-webapp.sh
+++ azure-webapp/azure-webapp.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # Batch modification of web app configurations
-az webapp config set --name <app_name> --resource-group <resource_group> --ftps-state Disabled --output none
+# az webapp config set --name <app_name> --resource-group <resource_group> --ftps-state Disabled --out table
 
 # define a arr
-array_var=('test1' 'test2' 'test3' 'test4')
-array_var[4]="test5"
 
-echo ${array_var[2]}
-echo ${array_var[3]}
-echo ${array_var[4]}
 
-echo ${array_var[*]}
+for i in $(seq 6)
+do
+  echo $app_test_services[$i]
+done
